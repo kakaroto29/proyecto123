42daea02624075e4d0c7017315787cc48cb71ecf
kakaroto29 <144288818+kakaroto29@users.noreply.github.com>
2024-04-15 16:50:36 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+
+function preload() {
+
+}
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+}
+
+function modelLoaded() {
+
+}
+
+function gotPoses(results)
+{
+ 
+  
+}
+
+function draw() {
+  
+ 
+}
+
+function take_snapshot(){    
+Save('myFilterImage.png');
+}
